517abc67a8f14494c1e259695ab3f2af71fbb1c5
YaraGh <yaraesmaiel123@gmail.com>
2025-01-26 08:18:01 -0600

    Add project files.

---
+++ Revision MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Revision_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
